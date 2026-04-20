03b760f8ca8041bd2305935b3e5ab41c442ed1d4
23091397085_M. SYAFI'UL MASRURI <149138466+23091397085-MSYAFIULMASRURI@users.noreply.github.com>
2026-04-21 06:30:56 +0700

    fix cors v2

--- server.js
+++ server.js
@@ -3,35 +3,18 @@ import cors from 'cors';
 import router from './src/routes.js';
 
 const app = express();
-const port = process.env.PORT || 3000;
-
-const allowedOrigins = [
-  'http://localhost:5173', // Vite dev local
-  'https://starter-project-express-ai-frontend.vercel.app/', // Preview URL Vercel
-  'https://starter-project-express-ai-git-64bf51-msyafiulmasruris-projects.vercel.app', // URL Vercel kamu
-  'https://starter-project-express-ai-frontend-o47fh28mn.vercel.app', // Preview URL Vercel
-];
+const port = 3000;
+const host = 'localhost';
 
 app.use(express.json());
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // Allow requests with no origin (Postman, curl, server-to-server)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error(`CORS blocked: ${origin}`));
-      }
-    },
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
+    origin: '*',
   })
 );
 
 app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Server running at port ${port}`);
+  console.log(`Server running at http://${host}:${port}`);
 });
